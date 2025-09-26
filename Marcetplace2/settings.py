6d892041a1477555d8b1dd6268e1b3826f9b5575
Mohamed Alkitony <mohamedalkitony23@gmail.com>
2025-09-26 20:52:50 +0300

    Update debur

--- Marcetplace2/settings.py
+++ Marcetplace2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pqwt%*=vs4=kq-vui80aa4su8736o^#vbe9llr6jhf2waa5(gg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
